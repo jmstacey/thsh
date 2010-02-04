a127e80646d9b4c2bbf50587f36feaff2d004eb3
Jon Stacey <jon@jon-laptop.(none)>
2010-02-04 15:52:27 -0600

    Initial commit

---
+++ src/thsh.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : thsh.c
+ Author      : Jon Stacey
+ Version     :
+ Copyright   : Copyright 2009 Jon Stacey. All rights reserved.
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
