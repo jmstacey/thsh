52ca822cd019b7b25e1a576d50c287d4257a5d78
Jon Stacey <jon@jon-laptop.(none)>
2010-02-16 14:52:29 -0600

    Start of basic program structure. Simple user input test

--- src/thsh.c
+++ src/thsh.c
@@ -13,10 +13,26 @@
 #include <stdlib.h>
 #include <signal.h>
 
+// TODO: Determine appropriate buffer size.
+int BUFFER_SIZE = 100; // Input buffer size (characters).
+
 int main(void)
 {
 	setvbuf(stdout, NULL, _IONBF, 0); // Disable buffering. See http://homepages.tesco.net/J.deBoynePollard/FGA/capture-console-win32.html
 	signal(SIGINT, SIG_IGN); 		  // Ignore ctrl-c. See http://www.cs.cf.ac.uk/Dave/C/node24.html
+
+	char input[BUFFER_SIZE];
+
+	while (1)
+	{
+		fgets(input, BUFFER_SIZE, stdin); // Get user input
+
+		printf("String: %s", input);
+
+		// Get user input
+		// Parse input
+		// Switch statement
+	}
 
 	return EXIT_SUCCESS;
 }
