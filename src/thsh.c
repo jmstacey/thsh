765836a4eabe15a8f11527dc403495f9b68671cc
Jon Stacey <jon@jon-laptop.(none)>
2010-02-16 14:33:42 -0600

    Removed hello world echo

--- src/thsh.c
+++ src/thsh.c
@@ -18,6 +18,5 @@ int main(void)
 	setvbuf(stdout, NULL, _IONBF, 0); // Disable buffering. See http://homepages.tesco.net/J.deBoynePollard/FGA/capture-console-win32.html
 	signal(SIGINT, SIG_IGN); 		  // Ignore ctrl-c. See http://www.cs.cf.ac.uk/Dave/C/node24.html
 
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
 	return EXIT_SUCCESS;
 }
