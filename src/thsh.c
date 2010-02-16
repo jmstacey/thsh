ae193c0aadeaa9be7e65afa87268436ee582fdb3
Jon Stacey <jon@jon-laptop.(none)>
2010-02-16 13:58:41 -0600

    Disable buffering to overcome apparent Eclipse bug

--- src/thsh.c
+++ src/thsh.c
@@ -12,6 +12,8 @@
 #include <stdlib.h>
 
 int main(void) {
+	setvbuf(stdout, NULL, _IONBF, 0); // Disable buffering in accordance to http://homepages.tesco.net/J.deBoynePollard/FGA/capture-console-win32.html
+
 	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
 	return EXIT_SUCCESS;
 }
