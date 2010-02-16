08e64f161e959410be6fb379921ea59a6e7faac7
Jon Stacey <jon@jon-laptop.(none)>
2010-02-16 14:29:19 -0600

    Ignore ctrl-c interrupt

--- src/thsh.c
+++ src/thsh.c
@@ -11,9 +11,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
 
 int main(void) {
-	setvbuf(stdout, NULL, _IONBF, 0); // Disable buffering in accordance to http://homepages.tesco.net/J.deBoynePollard/FGA/capture-console-win32.html
+	setvbuf(stdout, NULL, _IONBF, 0); // Disable buffering. See http://homepages.tesco.net/J.deBoynePollard/FGA/capture-console-win32.html
+	signal(SIGINT, SIG_IGN); 		  // Ignore ctrl-c. See http://www.cs.cf.ac.uk/Dave/C/node24.html
 
 	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
 	return EXIT_SUCCESS;
