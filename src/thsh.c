54ed8514134558d13ab53b81f9d94836e036f351
Collin <collin@collin-laptop.(none)>
2010-02-16 13:58:27 -0600

    Add environment vars and commandline arguments

--- src/thsh.c
+++ src/thsh.c
@@ -11,7 +11,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+extern char **environ;
+
+void printEnVars()
+{
+	char **env = environ;
+	printf("\n");
+	printf("%s\n", "--These are the environmental variables--");
+
+	while(*env)
+	{
+		printf("%s\n", *env);
+		env++;
+	}
+}
+
+int main(int argc, char *argv[], char *envp[])
+{
+/////////////////////////////////////////////////////// - Print Args
+	printf("%s\n", "--This is the total number of args--");
+	printf("%s", "argc | ");
+	printf("%i\n", argc);
+
+	printf("\n");
+
+	printf("%s\n", "--These are the argument characters--");
+	int i;
+	for (i = 0; i < argc; i++) {
+		printf("%i", i);
+		printf("%s", " | ");
+		printf("%s\n", argv[i]);
+	}
+
+/////////////////////////////////////////////////////// - Print all Environment Variables
+	printEnVars();
+
+/////////////////////////////////////////////////////// - Finished
+	printf("\n");
+	printf("%s\n", "Exited Program");
+
+	return 0;
 }
